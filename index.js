c75c12580d2fc14d8e78dbab7f06f3447b1d5472
Austin Vichich <avichich4@gmail.com>
2023-10-30 18:33:59 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+const random = Math.ceil(Math.random() * 1);
+console.log(random);
+
+let num3 = 4
+let num4 = 22
+let mod = num3 % num4;
+
+console.log(mod);
+
+const max = Math.max(1,4,5,7,8,20);
+console.log(max)
+
